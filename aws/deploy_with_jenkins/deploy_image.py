9380ddecac96f837caf44e0d78b9babdbd603488
Vlad Sanyuk <vlad.sanyuk@gmail.com>
2023-06-24 20:45:04 +0300

    Python in Jenkins pipeline exercise

--- aws/deploy_with_jenkins/deploy_image.py
+++ aws/deploy_with_jenkins/deploy_image.py
@@ -51,9 +51,8 @@ for ip in instances_public_ip:
     print(f"Logging into server {ip}")
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # create key object from env var value, StringIO is required to convert env var value into file-like object, that is expected by paramiko
-    key = paramiko.RSAKey.from_private_key(io.StringIO(ssh_key))
-    ssh.connect(hostname=ip, username="ubuntu", pkey=key)
+    # key = paramiko.RSAKey.from_private_key(ssh_key)
+    ssh.connect(hostname=ip, username="ubuntu", pkey=ssh_key)
     ecr = boto3.client('ecr', region_name=region)
     token = ecr.get_authorization_token()['authorizationData'][0]['authorizationToken']
     stdin, stdout, stderr = ssh.exec_command(f'echo {token} | sudo docker login -u AWS --password-stdin && sudo docker run --name {app_name} -p 8080:80 {image}')
