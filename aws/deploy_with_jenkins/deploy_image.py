4c23e8845b083d983e6103d8a216403674646cd5
Vlad Sanyuk <vlad.sanyuk@gmail.com>
2023-06-29 10:45:18 +0300

    Python in Jenkins pipeline exercise

--- aws/deploy_with_jenkins/deploy_image.py
+++ aws/deploy_with_jenkins/deploy_image.py
@@ -6,7 +6,7 @@ import sys
 import requests
 
 app_name = os.getenv("APP")
-ssh_key = os.getenv("SSH_KEY")
+# ssh_key = os.getenv("SSH_KEY")
 image = os.getenv("IMAGE")
 region = os.getenv("REGION")
 
@@ -51,7 +51,7 @@ for ip in instances_public_ip:
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     # key = paramiko.RSAKey.from_private_key(paramiko.RSAKey(data=ssh_key.encode()))
-    ssh.connect(hostname=ip, username="ubuntu", pkey=ssh_key)
+    ssh.connect(hostname=ip, username="ubuntu", key_filename="./public-key.pub")
     ecr = boto3.client('ecr', region_name=region)
     token = ecr.get_authorization_token()['authorizationData'][0]['authorizationToken']
     stdin, stdout, stderr = ssh.exec_command(f'echo {token} | sudo docker login -u AWS --password-stdin && sudo docker run --name {app_name} -p 8080:80 {image}')
